d77af33f9c4ff78c4bd9ebb068937c842d00fc35
EvilMoonBerry <petra.m.sjogren@gmail.com>
2022-09-22 17:38:32 +0300

    more data
    added more and changed titles

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import "./styles.css";
+
+//Maker Petra
+// Got help from course assistant and used course material
 let table = document.getElementById("table");
 let tbody = document.getElementById("tbody");
 let thead = document.getElementById("thead");
-//let body = document.getElementById("body");
 
 let title1 = document.createElement("th");
 let title2 = document.createElement("th");
-title1.innerText = "municipality";
+let title3 = document.createElement("th");
+title1.innerText = "Municipality";
 thead.appendChild(title1);
-title2.innerText = "population";
+title2.innerText = "Population";
 thead.appendChild(title2);
+title3.innerText = "Employment";
+thead.appendChild(title3);
 
 table.appendChild(thead);
 table.appendChild(tbody);
 
-//body.appendChild(table);
+getData();
 
 async function getData() {
   const url =
@@ -23,6 +28,13 @@ async function getData() {
   const data = await dataPromise.json();
   const cityData = data.dataset.dimension.Alue.category.label;
   const cityValueJSON = data.dataset.value;
+
+  const url2 =
+    "https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065";
+  const empPromise = await fetch(url2);
+  const empJSON = await empPromise.json();
+  const empValue = empJSON.dataset.value;
+
   // got help from course assistent with data and loop
   let x = Object.values(cityData);
 
@@ -30,13 +42,14 @@ async function getData() {
     let row = document.createElement("tr");
     let cell1 = document.createElement("td");
     let cell2 = document.createElement("td");
+    let cell3 = document.createElement("td");
 
     cell1.innerText = x[i];
     cell2.innerText = cityValueJSON[i];
+    cell3.innerText = empValue[i];
     row.appendChild(cell1);
     row.appendChild(cell2);
+    row.appendChild(cell3);
     tbody.appendChild(row);
   }
 }
-
-getData();
